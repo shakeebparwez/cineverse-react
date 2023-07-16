cd12501f7b5c0dc42e44fd59e1e83af5ab237c67
Shakeeb Parwez <shakeebparwez@gmail.com>
2023-07-17 04:59:43 +0530

    Build app structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import './App.css'
+import './App.css';
+import { AllRoutes } from "./routes/AllRoutes";
 
 function App() {
   return (
-    <>
-
-    </>
+    <div className="App">
+      <AllRoutes />
+    </div>
   )
 }
 
