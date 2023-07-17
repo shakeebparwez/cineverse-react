2c955e257035e12d699ef3480e059a178064743f
Shakeeb Parwez <shakeebparwez@gmail.com>
2023-07-18 03:08:48 +0530

    Add header and footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,23 @@
+import { Link } from "react-router-dom";
+
 export const Footer = () => {
   return (
-    <footer>Footer</footer>
+      <footer className="p-4 bg-white shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
+          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <Link to="/" className="hover:underline">CineVerse</Link>. All Rights Reserved.</span>
+          <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
+              <li>
+                  <a href="#" target="_blank" className="mr-4 hover:underline md:mr-6 ">Instagram</a>
+              </li>
+              <li>
+                  <a href="#" target="_blank" className="mr-4 hover:underline md:mr-6">LinkedIn</a>
+              </li>
+              <li>
+                  <a href="#" target="_blank" className="mr-4 hover:underline md:mr-6">Youtube</a>
+              </li>
+              <li>
+                  <a href="#" target="_blank" className="hover:underline">Github</a>
+              </li>
+          </ul>
+      </footer>
   )
 }
