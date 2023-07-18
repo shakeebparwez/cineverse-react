6c30a2211a6803f2e894a43c87be9b1eb517d6a9
Shakeeb Parwez <shakeebparwez@gmail.com>
2023-07-19 02:54:11 +0530

    Style and link update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,20 +3,24 @@ import { Link } from "react-router-dom";
 export const Footer = () => {
   return (
       <footer className="p-4 bg-white shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
-          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <Link to="/" className="hover:underline">CineVerse</Link>. All Rights Reserved.</span>
+          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2030 <Link to="/" className="hover:underline">Cinemate</Link>. All Rights Reserved.</span>
           <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
               <li>
-                  <a href="#" target="_blank" className="mr-4 hover:underline md:mr-6 ">Instagram</a>
+                  <a href="https://www.instagram.com/shakeebparwez" target="_blank" rel="noreferrer" className="mr-4 hover:underline md:mr-6 ">Instagram</a>
               </li>
               <li>
-                  <a href="#" target="_blank" className="mr-4 hover:underline md:mr-6">LinkedIn</a>
+                  <a href="https://www.linkedin.com/in/shakeebparwez" target="_blank" rel="noreferrer" className="mr-4 hover:underline md:mr-6">LinkedIn</a>
               </li>
               <li>
-                  <a href="#" target="_blank" className="mr-4 hover:underline md:mr-6">Youtube</a>
+                  <a href="https://twitter.com/shakeebparwez" target="_blank" rel="noreferrer" className="mr-4 hover:underline md:mr-6">Twitter</a>
               </li>
               <li>
-                  <a href="#" target="_blank" className="hover:underline">Github</a>
+                  <a href="https://www.youtube.com/channel/UCSMcXmGNIEON5sB_E9XKZYQ" target="_blank" rel="noreferrer" className="mr-4 hover:underline md:mr-6">YouTube</a>
               </li>
+              <li>
+                  <a href="https://github.com/shakeebparwez" target="_blank" rel="noreferrer" className="hover:underline">GitHub</a>
+              </li>
+              
           </ul>
       </footer>
   )
