2a5a424ac42e7a79527256d29e23c81af2d049bd
Shakeeb Parwez <shakeebparwez@gmail.com>
2023-07-19 02:26:17 +0530

    Scroll to top feature

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,13 @@ import './index.css';
 
 import { BrowserRouter as Router } from 'react-router-dom';
 
+
+import { ScrollToTop } from "./components";
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
+      <ScrollToTop />
       <App />
     </Router>
   </React.StrictMode>,
